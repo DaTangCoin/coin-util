d9d26a32f401b0fb11e2402e17adca72436d0ac6
Yu Sun <yu.sun.cs@gmail.com>
2017-07-14 15:50:26 -0700

    json

--- src/main/java/com/datang/coin/Coin.java
+++ src/main/java/com/datang/coin/Coin.java
@@ -1,5 +1,8 @@
 package com.datang.coin;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Coin {
 
 	private String id;
@@ -13,93 +16,93 @@ public class Coin {
 	private String quantity;
 	private String imageUrlLarge;
 	private String imageUrlSmall;
-	
-	
+
+
 
 	public void setId(String id){
 		this.id = id;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setYear(String year) {
 		this.year = year;
 	}
-	
+
 	public void setSize(String size) {
 		this.size = size;
 	}
-	
+
 	public void setPrice(String price){
 		this.price = price;
 	}
-	
+
 	public void setPrice68(String price){
 		this.price68 = price;
 	}
-	
+
 	public void setPrice69(String price){
 		this.price69 = price;
 	}
-	
+
 	public void setPrice70(String price){
 		this.price70 = price;
 	}
-	
+
 	public void setQuantity(String quantity){
 		this.quantity = quantity;
 	}
-	
+
 	public void setImageUrlLarge(String url){
 		this.imageUrlLarge = url;
 	}
-	
+
 	public void setImageUrlSmall(String url){
 		this.imageUrlSmall = url;
 	}
-	
+
 	public String getId(){
 		return id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getYear() {
 		return year;
 	}
-	
+
 	public String getSize(){
 		return size;
 	}
-	
+
 	public String getPrice() {
 		return price;
 	}
-	
+
 	public String getPrice68(){
 		return price68;
 	}
-	
+
 	public String getPrice69(){
 		return price69;
 	}
-	
+
 	public String getPrice70(){
 		return price70;
 	}
-	
+
 	public String getQuantity(){
 		return quantity;
 	}
-	
+
 	public String getImageUrlLarge(){
 		return imageUrlLarge;
 	}
-	
+
 	public String getImageUrlSmall(){
 		return imageUrlSmall;
 	}
